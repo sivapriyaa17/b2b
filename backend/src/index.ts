08ddf92d75a04be712082f56a2ca6829da9cdc8b
sivapriyaa <sivapriyaa2006@gmail.com>
2025-07-03 22:50:01 +0530

    Fix tender changes

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -19,7 +19,7 @@ const PORT = process.env.PORT || 5000;
 app.use(express.json());
 
 app.use(cors({
-    origin: ['http://localhost:3000','https://b2b-beta-weld.vercel.app/'],
+    origin: ['http://localhost:3000','https://b2b-beta-weld.vercel.app/','https://b2b-h94kw5kmn-sivapriyaas-projects.vercel.app'],
     credentials: true,
   }));
   
