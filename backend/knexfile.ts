b362d790e9d9385a80e01e84959a6b4397c750bb
sivapriyaa <sivapriyaa2006@gmail.com>
2025-07-03 13:54:42 +0530

    Fix knexfile

--- backend/knexfile.ts
+++ backend/knexfile.ts
@@ -4,13 +4,8 @@ import type { Knex } from 'knex';
 const config: { [key: string]: Knex.Config } = {
   development: {
     client: 'pg',
-    connection: {
-      host: '127.0.0.1',
-      port: 5432, // Ensure this is correct
-      user: 'postgres', // your DB username
-      password: 'hello@2027', // your DB password
-      database: 'tenders', // your DB name
-    },
+    connection: process.env.DATABASE_URL,
+
     migrations: {
       directory: './migrations',
       extension: 'ts',
