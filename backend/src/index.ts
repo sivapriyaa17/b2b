d1809f1e2926005e85d2967fc00c29127cecb143
sivapriyaa <sivapriyaa2006@gmail.com>
2025-07-03 23:41:52 +0530

    Fix: tenders

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -19,7 +19,8 @@ const PORT = process.env.PORT || 5000;
 app.use(express.json());
 
 app.use(cors({
-    origin: ['http://localhost:3000','https://b2b-beta-weld.vercel.app/','https://b2b-ixn4jizzx-sivapriyaas-projects.vercel.app/'],
+    origin: ['http://localhost:3000','https://b2b-beta-weld.vercel.app','https://b2b-ixn4jizzx-sivapriyaas-projects.vercel.app'],
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
     credentials: true,
   }));
   
